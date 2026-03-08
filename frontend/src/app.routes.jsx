3ff310a79e4efc5f4a507fa29aae14126f915223
aditya6002 <adityaofficial690@gmail.com>
2026-03-08 15:23:56 +0530

    improve home page

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -14,6 +14,19 @@ export const router = createBrowserRouter([
   },
   {
     path: "/",
-    element: <Protected children={<h1>Home Route</h1>} />,
+    element: (
+      <Protected
+        children={
+          <main>
+            <div className="">
+              {" "}
+              <h1>Welcome to the protected route!</h1>
+              <h2>Home Page</h2>
+            </div>
+            <a href="http://localhost:8080/api/auth/logout">Logout</a>
+          </main>
+        }
+      />
+    ),
   },
 ]);
