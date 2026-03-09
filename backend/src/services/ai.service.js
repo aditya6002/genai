1ac22740dcd07f88ea1b72fcbe79b24b390d6155
aditya6002 <adityaofficial690@gmail.com>
2026-03-09 10:05:22 +0530

    feat: update AI service initialization and fix import path in auth routes

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,7 +1,20 @@
-const { GoogleGenAI } = require("google-genai");
+const { GoogleGenAI } = require("@google/genai");
+require("dotenv").config();
 
+// Initialize the Google GenAI client with your API key
 const ai = new GoogleGenAI({
   apiKey: process.env.GOOGLE_GENAI_API_KEY,
 });
 
-async function invokeGeminiAi(model, contents) {}
+// Function to invoke the Gemini AI model with the provided contents
+async function invokeGeminiAi(contents) {
+  const res = await ai.models.generateContent({
+    model: process.env.GOOGLE_GENAI_MODEL,
+    contents: contents,
+  });
+  return res.text;
+}
+
+module.exports = {
+  invokeGeminiAi,
+};
