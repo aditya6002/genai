55475f3888a2b4f3741b60dc1c6978fe30d22797
aditya6002 <adityaofficial690@gmail.com>
2026-03-10 11:06:32 +0530

    Home page

--- frontend/src/features/interview/pages/Home.jsx
+++ frontend/src/features/interview/pages/Home.jsx
@@ -1,27 +1,42 @@
 import React from "react";
+import "../home.scss";
 
 const Home = () => {
   return (
     <main className="home">
-      <div className="left">
-        <textarea
-          name="jobDescription"
-          id="jobDescription"
-          placeholder="Enter job description"
-        ></textarea>
-      </div>
-      <div className="right">
-        <div className="input-group">
-          <label htmlFor="resume">Upload Resume</label>
-          <input type="file" id="resume" name="resume" accept=".pdf" />
-        </div>
-        <div className="input-group">
-          <label htmlFor="selfDescription">Self Description</label>
+      <div className="interview-input-group">
+        <div className="left">
+          <label htmlFor="jobDescription">Job Description</label>
           <textarea
-            name="selfDescription"
-            id="selfDescription"
-            placeholder="Describe yourself in a few sentences"
+            name="jobDescription"
+            id="jobDescription"
+            placeholder="Enter job description"
           ></textarea>
+        </div>
+        <div className="right">
+          <div className="input-group">
+            <p>
+              Resume{" "}
+              <small className="highlight">
+                (Use Resume and self description together for best results)
+              </small>
+            </p>
+            <label htmlFor="resume" id="file-label">
+              Upload Resume
+            </label>
+            <input type="file" hidden id="resume" name="resume" accept=".pdf" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="selfDescription">Self Description</label>
+            <textarea
+              name="selfDescription"
+              id="selfDescription"
+              placeholder="Describe yourself in a few sentences"
+            ></textarea>
+            <button className="button primary-button">
+              Generate Interview Report
+            </button>
+          </div>
         </div>
       </div>
     </main>
